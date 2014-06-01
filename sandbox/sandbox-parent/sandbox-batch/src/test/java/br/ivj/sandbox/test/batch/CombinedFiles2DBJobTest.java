2019446c90ec80c9308751f5e372c2f841e55378
itajun <itajun@gmail.com>
2014-06-01 14:01:11 -0300

    Mais exemplos de batch

--- sandbox/sandbox-parent/sandbox-batch/src/test/java/br/ivj/sandbox/test/batch/CombinedFiles2DBJobTest.java
+++ sandbox/sandbox-parent/sandbox-batch/src/test/java/br/ivj/sandbox/test/batch/CombinedFiles2DBJobTest.java
@@ -1,40 +1,54 @@
 package br.ivj.sandbox.test.batch;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.StepScopeTestExecutionListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+
+import br.ivj.sandbox.service.BookService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/app-context-service.xml",
 		"/app-context-persistence-test.xml", "/app-context-batch-test.xml",
 		"/jobs/combined-files2db-job.xml" })
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-		StepScopeTestExecutionListener.class })
+		StepScopeTestExecutionListener.class,
+		DirtiesContextTestExecutionListener.class })
 @ActiveProfiles(profiles = { "development" })
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 public class CombinedFiles2DBJobTest {
-	/*
-	 * TODO: Why am I getting duplicated bean errors when I run all tests in this package?
-	 * javax.naming.NamingException: Another resource already exists with name sandboxDatabase - pick a different name
-	 */
-	
 	@Autowired
 	private JobLauncherTestUtils jobLauncher;
+
+	@Autowired
+	private BookService bookService;
 
 	@Autowired
 	private Job job;
 
 	@Test
 	public void expectedSuccessfulImport() throws Exception {
+		assertNull(bookService.findBookById(6));
+
 		jobLauncher.setJob(job);
-		jobLauncher.launchJob(new JobParameters());
+		JobExecution jobResult = jobLauncher.launchJob(new JobParameters());
+		assertEquals(jobResult.getExitStatus(), ExitStatus.COMPLETED);
 	}
+
 }
--- sandbox/sandbox-parent/sandbox-batch/src/test/java/br/ivj/sandbox/test/batch/XML2CSVJobTest.java
+++ sandbox/sandbox-parent/sandbox-batch/src/test/java/br/ivj/sandbox/test/batch/XML2CSVJobTest.java
@@ -1,25 +1,33 @@
 package br.ivj.sandbox.test.batch;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.StepScopeTestExecutionListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "/app-context-service.xml",
 		"/app-context-persistence-test.xml", "/app-context-batch-test.xml",
 		"/jobs/xml2csv-job.xml" })
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-		StepScopeTestExecutionListener.class })
+	StepScopeTestExecutionListener.class, DirtiesContextTestExecutionListener.class})
 @ActiveProfiles(profiles = { "development" })
+@DirtiesContext(classMode=ClassMode.AFTER_CLASS)
 public class XML2CSVJobTest {
 	@Autowired
 	private JobLauncherTestUtils jobLauncher;
@@ -27,9 +35,11 @@ public class XML2CSVJobTest {
 	@Autowired
 	private Job job;
 
+
 	@Test
 	public void expectedSuccessfulImport() throws Exception {
 		jobLauncher.setJob(job);
-		jobLauncher.launchJob(new JobParameters());
+		JobExecution jobExecution = jobLauncher.launchJob(new JobParameters());
+		assertEquals(jobExecution.getExitStatus(), ExitStatus.COMPLETED);
 	}
 }
